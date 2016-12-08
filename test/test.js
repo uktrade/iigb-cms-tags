2d5b34e756fcfb893a02af0633eb5af7dec952f4
breneser <brhneser@gmail.com>
2016-12-08 16:05:30 +0000

    cms tag parser first iteration

--- test/test.js
+++ test/test.js
@@ -1,9 +1,8 @@
 var fs = require('fs');
-var html = fs.readFileSync('test.html', 'utf-8').toString();
+var html = fs.readFileSync(__dirname + '/test.html', 'utf-8').toString();
 var cmsField = require('..');
 var content =  cmsField.parse(html);
 
 
+
 console.log(content);
-
-
